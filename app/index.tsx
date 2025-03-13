ee3dc23140d02edde99cb9f298f09d28510fee4f
everjosue56 <josueychurr9@gmail.com>
2025-03-13 16:57:29 -0600

    primer commit, subir archivos

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-center color-blue-800 mt-10 '>Haciendo Pruebas jasjas</Text>
+    </View>
+  )
+}
+
+export default index
